207762759c539dabbd08865aa92426ac3dd1200b
PSKpskPSK <105923614+PSKpskPSK@users.noreply.github.com>
2022-05-20 20:08:30 +0800

    Add files via upload

---
+++ 14_Amirul_PESS/nav.php
@@ -0,0 +1,20 @@
+<header style="background-image: url('images/banner.jpg'); height: 230px;background-size: cover;"></header>
+
+<nav class="navbar navbar-expand-sm bg-light">
+
+ <!-- Links -->
+ 	<ul class="navbar-nav">
+ 		<li class="nav-item">
+ 		<a class="nav-link" href="logcall.php">Log Call</a>
+		</li>
+ 		<li class="nav-item">
+ 		<a class="nav-link" href="search.php">Update</a>
+		</li>
+ 		<li class="nav-item">
+ 		<a class="nav-link" href="#">Report</a>
+		</li>
+ 		<li class="nav-item">
+ 		<a class="nav-link" href="#">Admin</a>
+		</li>
+ 	</ul>
+ </nav>
